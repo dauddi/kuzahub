cb3ea1c9743d8395b9bdd1a489482ede2792fdc1
Davis <kuntaytc@gmail.com>
2021-11-09 00:37:17 +0300

    mui avatar component

---
+++ components/common/ImgAvatar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Avatar } from '@mui/material';
+
+const ImgAvatar = () => {
+    return (
+        <Avatar alt="me" src="public/images/me.jpg" />
+    )
+}
+
+export default ImgAvatar
