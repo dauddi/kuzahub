d711557cc6cc70702f877c3a6437f0f8c111031a
Davis <kuntaytc@gmail.com>
2021-11-09 00:47:26 +0300

    initial supply page

---
+++ pages/ap/supply/index.jsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import styles from "../../../styles/Home.module.css"
+
+const Supply = () => {
+	return (
+		<div className={styles.main}>
+			<h1>Supply Homepage</h1>
+			<Link passHref href="/">
+				<h2>
+					&larr; Go back to Homepage
+				</h2>
+			</Link>
+		</div>
+	);
+};
+
+export default Supply;
