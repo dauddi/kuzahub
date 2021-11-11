21e258d56bc7ce7af0a478c1b89a7484ec2e1f63
Davis <kuntaytc@gmail.com>
2021-11-11 19:56:06 +0300

    added layout

--- pages/ap/marketplace/index.jsx
+++ pages/ap/marketplace/index.jsx
@@ -1,37 +1,26 @@
-import Link from "next/link";
 import React from "react";
 import styles from "./market.module.css"
-import ListingList from "../../../components/marketplace/ListingList";
-import Sidebar from "../../../components/marketplace/Sidebar"
-import Navbar from '../../../components/marketplace/Navbar'
-
+import ListingList from "../../../src/components/marketplace/ListingList";
+import Sidebar from "../../../src/components/marketplace/Sidebar"
+import Layout from "../../../src/components/layout"
 
 const Marketplace = () => {
 	return (
-		<div className={styles.container}>
-			<header className={styles.header}>
-				<Navbar />
-			</header>
+		<Layout Marketplace>
+			<div className={styles.container}>
+				<section className={styles.section}>
+					<aside className={styles.aside}>
+						<Sidebar />
+					</aside>
 
-			<section className={styles.section}>
-				<aside className={styles.aside}>
-					<Sidebar />
-				</aside>
-
-				<main className={styles.main}>
-					<h1>Feed</h1>
-					<ListingList />
-				</main>
-			</section>
-
-			<footer className={styles.footer}>
-				<Link passHref href="/">
-					<h2>
-						&larr; Go back to Homepage
-					</h2>
-				</Link>
-			</footer>			
-		</div>
+					<main className={styles.main}>
+						<h1>Feed</h1>
+						<ListingList />
+					</main>
+				</section>
+			</div>
+		</Layout>
+		
 	);
 };
 
