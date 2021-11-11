d16bf685c66842d81de0cb119ebf734a424568ee
Davis <kuntaytc@gmail.com>
2021-11-11 20:27:45 +0300

    updated redirect path

--- pages/api/login.js
+++ pages/api/login.js
@@ -5,22 +5,22 @@ import md5 from "md5";
 connect();
 
 const handler = async (req, res) => {
-	const { username: user_name, password } = JSON.parse(req.body);
+  const { username: user_name, password } = JSON.parse(req.body);
 
-	switch (req.method) {
-		case "POST": {
-			User.findOne({ username: user_name }, (err, foundUser) => {
-				if (!foundUser || foundUser.password !== md5(password)) {
-					return res.status(401).send({ error: "Invalid username/password" });
-				} else {
-					return res.redirect(307, "../ap/services");
-				}
-			});
-			break;
-		}
-		default:
-			return res.status(400).send({ error: "Invalid request" });
-	}
+  switch (req.method) {
+    case "POST": {
+      User.findOne({ username: user_name }, (err, foundUser) => {
+        if (!foundUser || foundUser.password !== md5(password)) {
+          return res.status(401).send({ error: "Invalid username/password" });
+        } else {
+          return res.redirect(307, "/ap/services");
+        }
+      });
+      break;
+    }
+    default:
+      return res.status(400).send({ error: "Invalid request" });
+  }
 };
 
 export default handler;
