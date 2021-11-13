d4719ffb33b4499a8d93b6e9687b02fb73105218
Davis <kuntaytc@gmail.com>
2021-11-13 06:06:41 +0300

    responsive navbar update

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import styles from './navbar.module.css'
 import SearchIcon from '@material-ui/icons/Search';
-import HeaderOption from './HeaderOption';
 import HomeIcon from '@material-ui/icons/Home';
 import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
 import BusinessCenterIcon from '@material-ui/icons/BusinessCenter';
@@ -11,17 +10,31 @@ import Image from 'next/image'
 import { useSelector, useDispatch } from 'react-redux'
 import { authenticate } from '../../features/Auth/authSlice'
 import Link from 'next/link'
+import PersonPinIcon from '@mui/icons-material/PersonPin';
+import ButtonPrimary from './ButtonPrimary'
+import Badge from '@mui/material/Badge';
+import Router from 'next/router'
+
+
 
 function Navbar() {
     const isAuthenticated = useSelector((state) => state.auth.isAuthenticated)
     const dispatch = useDispatch()
 
+    const handleCreateListing = () => {
+        if (isAuthenticated) {
+            Router.push('/ap/NewListing')
+        } else {
+            Router.push('/nu/register')
+        }
+    }
+    
     return (
         <div className={styles.header}>
 
             <div className={styles.header__left}>
                 <Link href="/" passHref >
-                    <Image priority src="/images/logo.png" width={160} height={43.4} alt="tuzahub logo" />
+                    <Image className="logo" priority src="/images/logo.png" width={160} height={43.4} alt="tuzahub logo" />
                 </Link>
                 
                 <div className={styles.header__search}>
@@ -30,13 +43,27 @@ function Navbar() {
                 </div>
             </div>
 
-            <div className={styles.header__right}>
-                <HeaderOption Icon = {HomeIcon} title = 'Home' />
-                <HeaderOption Icon = {SupervisorAccountIcon} title = 'My Network' />
-                <HeaderOption Icon = {ChatIcon} title = 'Messages' />
-                <HeaderOption Icon = {NotificationsIcon} title = 'Notifications' />
-                <HeaderOption avatar = "https://avatars.githubusercontent.com/u/64232602?v=4" title = 'Profile' />
-            </div>
+            { isAuthenticated && <div className={styles.header__right}>
+                <HomeIcon />
+                <Badge badgeContent={4} fontSize="small" color="warning" >
+                    <ChatIcon />
+                </Badge>
+
+                <Badge badgeContent={2} color="warning" >
+                    <NotificationsIcon />
+                </Badge>
+                
+                <Badge color='success' variant='dot'>
+                    <PersonPinIcon />
+                </Badge>
+            </div>}
+
+            {!isAuthenticated && <Link href="/ap/login" >
+                <a>Sign In</a>
+            </Link>}
+
+            <ButtonPrimary clickHandler={handleCreateListing} title="Create a Listing" />
+
         </div>
     )
 }
