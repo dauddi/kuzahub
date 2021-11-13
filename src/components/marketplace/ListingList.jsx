66973237f3eb8cfa5edfbdcaf5f315e1442b893f
Davis <kuntaytc@gmail.com>
2021-11-13 06:09:39 +0300

    updated listingCard component

--- src/components/marketplace/ListingList.jsx
+++ src/components/marketplace/ListingList.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import ListingItem from './ListingItem'
 import styles from "./styles.module.css"
 import listings from '../../../listings.json'
+import ListingCard from './ListingCard'
 
 const ListingList = () => {
     return (
         <div className={styles.listings}>
-            { listings.map(listing => <ListingItem listing={listing} key={listing.id} /> ) }
+            { listings.map(listing => <ListingCard listing={listing} key={listing.id} /> ) }
         </div>
     )
 }
