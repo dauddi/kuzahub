17e183b79f584d12725c9bbc121e467b21df1f15
Davis Okioma <kuntaytc@gmail.com>
2021-11-17 04:58:21 +0300

    demo campaigns component

---
+++ src/components/common/CampaignBanner.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './campaignbanner.module.scss'
+
+const CampaignBanner = () => {
+    return (
+        <div className={styles.chimpBanner}>
+            <h3> MailChimp Campaign </h3>
+        </div>
+    )
+}
+
+export default CampaignBanner;
