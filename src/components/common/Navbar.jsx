241620f14b84a218c77750ee20b1d1710b323384
Davis Okioma <kuntaytc@gmail.com>
2021-11-18 14:11:41 +0300

    updated navbar

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import styles from './navbar.module.scss'
 import SearchIcon from '@material-ui/icons/Search';
 import HomeIcon from '@material-ui/icons/Home';
@@ -6,6 +6,7 @@ import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
 import BusinessCenterIcon from '@material-ui/icons/BusinessCenter';
 import ChatIcon from '@material-ui/icons/Chat';
 import NotificationsIcon from '@material-ui/icons/Notifications';
+import Avatar from '@mui/material/Avatar';
 import Image from 'next/image'
 import { useSelector, useDispatch } from 'react-redux'
 import { authenticate } from '../../features/Auth/authSlice'
@@ -13,84 +14,139 @@ import Link from 'next/link'
 import PersonPinIcon from '@mui/icons-material/PersonPin';
 import ButtonPrimary from './ButtonPrimary'
 import Badge from '@mui/material/Badge';
+import { Divider } from '@material-ui/core';
 import Router from 'next/router'
+import FeaturedPlayListOutlinedIcon from '@mui/icons-material/FeaturedPlayListOutlined';
+import BookmarksOutlinedIcon from '@mui/icons-material/BookmarksOutlined';
+import PeopleAltOutlinedIcon from '@mui/icons-material/PeopleAltOutlined';
+import ExitToAppOutlinedIcon from '@mui/icons-material/ExitToAppOutlined';
 
 import { useSession } from 'next-auth/react'
-
 import {signIn, signOut} from 'next-auth/react'
-
+import { SignalWifiConnectedNoInternet4 } from '@mui/icons-material';
 
 function Navbar() {
+    const [menuClicked, setMenuClicked] = useState(false)
+    const { data: session, status } = useSession();
+    const { user } = session ? session : {};
 
-    const {data: session, status} = useSession();
     console.log(session, status)
 
-    // const handleCreateListing = () => {
-    //     if (isAuthenticated) {
-    //         Router.push('/ap/NewListing')
-    //     } else {
-    //         Router.push('/nu/register')
-    //     }
-    // }
+    useEffect(() => {
+        
+    })
     
     return (
-        <div className={styles.header}>
-
-            <div className={styles.header__left}>
-                <Link href="/" passHref >
-                    <Image className="logo" priority src="/images/logo.png" width={160} height={43.4} alt="tuzahub logo" />
-                </Link>
-                
-                <div className={styles.header__search}>
-                    <SearchIcon fontSize="small" />
-                    <input type="text" />
+        <div className={styles.container}>
+            <div className={styles.header}>
+                <div className={styles.header__left}>
+                    <Link href="/" passHref >
+                        <Image className="logo" priority src="/images/logo.png" width={160} height={43.4} alt="tuzahub logo" />
+                    </Link>
+                    
+                    <div className={styles.header__search}>
+                        <SearchIcon fontSize="small" />
+                        <input type="text" />
+                    </div>
                 </div>
+
+                {(session && status === 'authenticated') && <div className={styles.header__right}>
+                    <Link href="/" passHref>
+                        <HomeIcon />
+                    </Link>
+                    
+
+                    <Badge badgeContent={2} color="warning" >
+                        <NotificationsIcon />
+                    </Badge>
+
+                    <div>
+                    
+                    <Badge color='success' variant='dot'>
+                        <Avatar onClick={() => {setMenuClicked(!menuClicked)}} fontSize="small" src={user?.image} alt={user?.name}  />
+                    </Badge>
+
+
+
+                    { menuClicked && <div className={styles.dropdown} >
+                        <div className={styles.profile}>
+                            <Avatar src={user?.image} />
+                            <span>
+                                <h3>{user?.name}</h3>
+                                <Link href='#'>Manage your Profile</Link>
+                            </span>
+                        </div>
+
+                        <Divider />
+
+                        <Link href='#' passHref>
+                            <div className={styles.dropitems}>
+                                <ChatIcon fontSize="small" />
+                                <p>Messages</p>
+                            </div>
+                        </Link>
+                        
+                        <Link href='#' passHref>
+                            <div className={styles.dropitems}>
+                                <FeaturedPlayListOutlinedIcon fontSize="small" />
+                                <p>My Listings</p>
+                            </div>
+                        </Link>
+
+                        <Link href='#' passHref>
+                            <div className={styles.dropitems}>
+                                <BookmarksOutlinedIcon fontSize="small" />
+                                <p>Saved</p>
+                            </div>
+                        </Link>
+
+                        <Link href='#' passHref>
+                            <div className={styles.dropitems}>
+                                <PeopleAltOutlinedIcon fontSize="small" />
+                                <p>Connections</p>
+                            </div>
+                        </Link>
+
+                        <Link onClick={(e) => {
+                            e.preventDefault();
+                            signOut();
+                        }} href='/api/auth/signout' passHref>
+                            <div className={styles.dropitems}>
+                                <ExitToAppOutlinedIcon fontSize="small" />
+                                <p>Sign Out</p>
+                            </div>
+                        </Link>
+                    </div>}
+                    </div>
+
+
+
+
+                </div>}
+
+                {!(session && status === 'authenticated')  && <Link href="/api/auth/signin" >
+                    <a onClick={e => {
+                        e.preventDefault();
+                        signIn();
+                    }}>Sign In</a>
+                </Link>}
+
+                {!(session && status === 'authenticated') && <Link href="/api/auth/signup" >
+                    <a onClick={e => {
+                        e.preventDefault();
+                        if (!(session && status === 'authenticated')) {
+                            signIn();
+                        }
+                        else{
+                            Router.push('/ap/NewListing')
+                        }
+                    }} >Create Listing</a>
+                </Link>}
+
+                
             </div>
 
-            {(session && status === 'authenticated') && <div className={styles.header__right}>
-                <HomeIcon />
-                <Badge badgeContent={4} fontSize="small" color="warning" >
-                    <ChatIcon />
-                </Badge>
-
-                <Badge badgeContent={2} color="warning" >
-                    <NotificationsIcon />
-                </Badge>
-                
-                <Badge color='success' variant='dot'>
-                    <PersonPinIcon />
-                </Badge>
-            </div>}
-
-            {!(session && status === 'authenticated')  && <Link href="/api/auth/signin" >
-                <a onClick={e => {
-                    e.preventDefault();
-                    signIn();
-                }}>Sign In</a>
-            </Link>}
-
-            {(session && status === 'authenticated') && <Link href="/api/auth/signin" >
-                <a onClick={e => {
-                    e.preventDefault();
-                    signOut();
-                }}>Sign Out</a>
-            </Link>}
-
-            {/* <ButtonPrimary clickHandler={handleCreateListing} title="Create a Listing" /> */}
-
-            <Link href="/api/auth/signup" >
-                <a onClick={e => {
-                    e.preventDefault();
-                    if (!(session && status === 'authenticated')) {
-                        signIn();
-                    }
-                    else{
-                        Router.push('/ap/NewListing')
-                    }
-                    
-                }} >Create Listing</a>
-            </Link>
-
+            
         </div>
     )
 }
