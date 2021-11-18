150260b0653af256a241374e17c90d975e55021a
Davis Okioma <kuntaytc@gmail.com>
2021-11-18 14:11:04 +0300

    create listing api route

--- pages/api/listings/new.js
+++ pages/api/listings/new.js
@@ -1,7 +1,34 @@
+import connect from "../../../utils/database";
+import Post from "../../../models/post";
+
+connect();
+
 const handler = async (req, res) => {
   switch (req.method) {
     case "POST": {
-      res.status(200);
+      const postObject = JSON.parse(req.body);
+
+      const newPost = new Post({
+        title: postObject.title,
+        desc: postObject.desc,
+        category: postObject.category,
+        location: postObject.location,
+        creationDate: Date.now(),
+      });
+
+      await newPost.save((err) => {
+        if (err) {
+          console.log("Post update failed");
+          return;
+        } else {
+          console.log("Post added successfully");
+        }
+      });
+      return res.status(200).send({ msg: "Post Created Successfully!!" });
+    }
+
+    default: {
+      res.status(400).send({ error: "Invalid Request!!" });
     }
   }
 };
