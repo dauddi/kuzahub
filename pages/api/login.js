a14cc10a19e8329107a3c7a39172ae8b769e6db0
Davis Okioma <kuntaytc@gmail.com>
2021-11-18 03:45:18 +0300

    no longer used. subed for magic link authentication

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,65 +1,65 @@
-import passport from "passport";
-import nextConnect from "next-connect";
-import { localStrategy } from "../../lib/passport-local";
-import { setLoginSession } from "../../lib/auth";
-import connect from "../../utils/database";
-
-connect();
-
-const authenticate = (method, req, res) => {
-  new Promise((resolve, reject) => {
-    passport.authenticate(method, { session: false }, (error, token) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(token);
-      }
-    })(req, res);
-  });
-
-  passport.use(localStrategy);
-
-  export default nextConnect()
-    .use(passport.initialize())
-    .post(async (req, res) => {
-      try {
-        const user = await authenticate("local", req, res);
-        // session is the payload to save in the token, it may contain basic info about the user
-        const session = { ...user };
-
-        await setLoginSession(res, session);
-
-        res.status(200).send({ done: true });
-      } catch (error) {
-        console.error(error);
-        res.status(401).send(error.message);
-      }
-    });
-};
-
+// import passport from "passport";
+// import nextConnect from "next-connect";
+// import { localStrategy } from "../../lib/passport-local";
+// import { setLoginSession } from "../../lib/auth";
 // import connect from "../../utils/database";
-// import User from "../../models/user";
-// import md5 from "md5";
 
 // connect();
 
-// const handler = async (req, res) => {
-//   const { username: user_name, password } = JSON.parse(req.body);
+// const authenticate = (method, req, res) => {
+//   new Promise((resolve, reject) => {
+//     passport.authenticate(method, { session: false }, (error, token) => {
+//       if (error) {
+//         reject(error);
+//       } else {
+//         resolve(token);
+//       }
+//     })(req, res);
+//   });
 
-//   switch (req.method) {
-//     case "POST": {
-//       User.findOne({ username: user_name }, (err, foundUser) => {
-//         if (!foundUser || foundUser.password !== md5(password)) {
-//           return res.status(401).send({ error: "Invalid username/password" });
-//         } else {
-//           return res.status(200).send({ success: "Auth success!!" });
-//         }
-//       });
-//       break;
-//     }
-//     default:
-//       return res.status(400).send({ error: "Invalid request" });
-//   }
+//   passport.use(localStrategy);
+
+//   export default nextConnect()
+//     .use(passport.initialize())
+//     .post(async (req, res) => {
+//       try {
+//         const user = await authenticate("local", req, res);
+//         // session is the payload to save in the token, it may contain basic info about the user
+//         const session = { ...user };
+
+//         await setLoginSession(res, session);
+
+//         res.status(200).send({ done: true });
+//       } catch (error) {
+//         console.error(error);
+//         res.status(401).send(error.message);
+//       }
+//     });
 // };
 
-// export default handler;
+// // import connect from "../../utils/database";
+// // import User from "../../models/user";
+// // import md5 from "md5";
+
+// // connect();
+
+// // const handler = async (req, res) => {
+// //   const { username: user_name, password } = JSON.parse(req.body);
+
+// //   switch (req.method) {
+// //     case "POST": {
+// //       User.findOne({ username: user_name }, (err, foundUser) => {
+// //         if (!foundUser || foundUser.password !== md5(password)) {
+// //           return res.status(401).send({ error: "Invalid username/password" });
+// //         } else {
+// //           return res.status(200).send({ success: "Auth success!!" });
+// //         }
+// //       });
+// //       break;
+// //     }
+// //     default:
+// //       return res.status(400).send({ error: "Invalid request" });
+// //   }
+// // };
+
+// // export default handler;
--- pages/api/register.js
+++ pages/api/register.js
@@ -1,34 +1,34 @@
-import connect from "../../utils/database";
-import User from "../../models/user";
-import { createUser } from "../../lib/user";
+// import connect from "../../utils/database";
+// import User from "../../models/user";
+// import { createUser } from "../../lib/user";
 
-connect();
+// connect();
 
-const handler = async (req, res) => {
-  switch (req.method) {
-    case "POST": {
-      const userObject = JSON.parse(req.body);
+// const handler = async (req, res) => {
+//   switch (req.method) {
+//     case "POST": {
+//       const userObject = JSON.parse(req.body);
 
-      await createUser(userObject);
-      // const newUser = new User({
-      //   firstname: userObject.firstname,
-      //   lastname: userObject.lastname,
-      //   email: userObject.username,
-      //   username: userObject.username,
-      //   password: md5(userObject.password),
-      // });
+//       await createUser(userObject);
+//       // const newUser = new User({
+//       //   firstname: userObject.firstname,
+//       //   lastname: userObject.lastname,
+//       //   email: userObject.username,
+//       //   username: userObject.username,
+//       //   password: md5(userObject.password),
+//       // });
 
-      // await newUser.save((err) => {
-      //   if (err) {
-      //     console.log("Update failed!!");
-      //     return;
-      //   } else console.log("New User added Successfully");
-      // });
-      return res.status(200).send({ msg: "User Created Successfully!!" });
-    }
-    default:
-      res.status(400).send({ error: "Invalid Request!!" });
-  }
-};
+//       // await newUser.save((err) => {
+//       //   if (err) {
+//       //     console.log("Update failed!!");
+//       //     return;
+//       //   } else console.log("New User added Successfully");
+//       // });
+//       return res.status(200).send({ msg: "User Created Successfully!!" });
+//     }
+//     default:
+//       res.status(400).send({ error: "Invalid Request!!" });
+//   }
+// };
 
-export default handler;
+// export default handler;
