80fda3664a740d781165d71435a7883a8948b0fe
Davis Okioma <kuntaytc@gmail.com>
2021-11-28 19:27:28 +0300

    updated navbar

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,11 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import styles from './navbar.module.scss'
-import SearchIcon from '@material-ui/icons/Search';
-import HomeIcon from '@material-ui/icons/Home';
 import ChatIcon from '@material-ui/icons/Chat';
-import NotificationsIcon from '@material-ui/icons/Notifications';
 import Avatar from '@mui/material/Avatar';
-import Image from 'next/image'
 import Link from 'next/link'
 import Badge from '@mui/material/Badge';
 import { Divider } from '@material-ui/core';
@@ -50,85 +46,78 @@ function Navbar() {
                         <a> Marketplace </a>
                     </Link>
 
-                    {/* <Badge badgeContent={2} color="warning" >
-                        <NotificationsIcon />
-                    </Badge> */}
 
                     <div>
-                    
-                    <Badge color='success' variant='dot'>
-                        <Avatar onClick={() => {setMenuClicked(!menuClicked)}} fontSize="small" src={user?.image} alt={user?.name}  />
-                    </Badge>
 
+                        <Badge color='success' variant='dot'>
+                            <Avatar onClick={() => {setMenuClicked(!menuClicked)}} fontSize="small" src={user?.image} alt={user?.name}  />
+                        </Badge>
 
-
-                    { menuClicked && <div className={styles.dropdown} >
-                        <div className={styles.profile}>
-                            <Avatar src={user?.image} />
-                            <span>
-                                <h3>{user?.name}</h3>
-                                <Link href='#'>Manage your Profile</Link>
-                            </span>
-                        </div>
-
-                        <Divider />
-
-                        <Link href='#' passHref>
-                            <div className={styles.dropitems}>
-                                <ChatIcon fontSize="small" />
-                                <p>Messages</p>
+                        { menuClicked && <div className={styles.dropdown} >
+                            <div className={styles.profile}>
+                                <Avatar src={user?.image} />
+                                <span>
+                                    <h3>{user?.name}</h3>
+                                    <Link href='#'>Manage your Profile</Link>
+                                </span>
                             </div>
-                        </Link>
-                        
-                        <Link href='#' passHref>
-                            <div className={styles.dropitems}>
-                                <FeaturedPlayListOutlinedIcon fontSize="small" />
-                                <p>My Listings</p>
-                            </div>
-                        </Link>
 
-                        <Link href='#' passHref>
-                            <div className={styles.dropitems}>
-                                <PeopleAltOutlinedIcon fontSize="small" />
-                                <p>Notifications</p>
-                            </div>
-                        </Link>
+                            <Divider />
 
-                        <Link href='#' passHref>
-                            <div className={styles.dropitems}>
-                                <BookmarksOutlinedIcon fontSize="small" />
-                                <p>Saved</p>
-                            </div>
-                        </Link>
+                            <Link href='#' passHref>
+                                <div className={styles.dropitems}>
+                                    <ChatIcon fontSize="small" />
+                                    <p>Messages</p>
+                                </div>
+                            </Link>
+                            
+                            <Link href='#' passHref>
+                                <div className={styles.dropitems}>
+                                    <FeaturedPlayListOutlinedIcon fontSize="small" />
+                                    <p>My Listings</p>
+                                </div>
+                            </Link>
 
-                        <Link href='#' passHref>
-                            <div className={styles.dropitems}>
-                                <PeopleAltOutlinedIcon fontSize="small" />
-                                <p>Connections</p>
-                            </div>
-                        </Link>
+                            <Link href='#' passHref>
+                                <div className={styles.dropitems}>
+                                    <PeopleAltOutlinedIcon fontSize="small" />
+                                    <p>Notifications</p>
+                                </div>
+                            </Link>
 
-                        <Link onClick={(e) => {
-                            e.preventDefault();
-                            signOut();
-                        }} href='/api/auth/signout' passHref>
-                            <div className={styles.dropitems}>
-                                <ExitToAppOutlinedIcon fontSize="small" />
-                                <p>Sign Out</p>
-                            </div>
-                        </Link>
-                    </div>}
+                            <Link href='#' passHref>
+                                <div className={styles.dropitems}>
+                                    <BookmarksOutlinedIcon fontSize="small" />
+                                    <p>Saved</p>
+                                </div>
+                            </Link>
+
+                            <Link href='#' passHref>
+                                <div className={styles.dropitems}>
+                                    <PeopleAltOutlinedIcon fontSize="small" />
+                                    <p>Connections</p>
+                                </div>
+                            </Link>
+
+                            <Link onClick={(e) => {
+                                e.preventDefault();
+                                signOut();
+                            }} href='/api/auth/signout' passHref>
+                                <div className={styles.dropitems}>
+                                    <ExitToAppOutlinedIcon fontSize="small" />
+                                    <p>Sign Out</p>
+                                </div>
+                            </Link>
+                        </div> }
+
                     </div>
 
-
-
-
-                </div>}
+                </div> }
 
                 {!(session && status === 'authenticated')  && <Link href="/api/auth/signin" >
                     <a onClick={e => {
                         e.preventDefault();
-                        signIn();
+                        Router.push('/ap/login')
                     }}>Sign In</a>
                 </Link>}
 
@@ -136,7 +125,7 @@ function Navbar() {
                     <a onClick={e => {
                         e.preventDefault();
                         if (!(session && status === 'authenticated')) {
-                            signIn();
+                            Router.push('/nu/register')
                         }
                         else{
                             Router.push('/ap/NewListing')
@@ -144,7 +133,6 @@ function Navbar() {
                     }} >Create Listing</a>
                 </Link>}
 
-                
             </div>
 
             
