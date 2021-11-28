61b6006a58fd8efbec36018c901388bad0681ce9
Davis Okioma <kuntaytc@gmail.com>
2021-11-28 19:26:25 +0300

    custom login form

--- pages/ap/login.jsx
+++ pages/ap/login.jsx
@@ -1,117 +1,61 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import Copyright from '../../src/components/common/Copyright'
-import Router from 'next/router'
+import React from 'react'
+import Link from 'next/link'
+import styles from './login.module.scss'
+import {Divider} from '@mui/material'
+import GoogleIcon from '@mui/icons-material/Google';
+import EmailIcon from '@mui/icons-material/Email';
+import {signIn, signOut} from 'next-auth/react'
 
-import { authenticate } from '../../src/features/Auth/authSlice'
-import { useDispatch } from 'react-redux'
-
-const theme = createTheme();
-
-const Login = () => {
-  const dispatch = useDispatch()
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-
-    const res = await fetch("/api/login", {
-      method: "POST", 
-      body: JSON.stringify({
-        username: data.get('username'),
-        password: data.get('password'),
-      }),
-    })
-    
-    if (res.status === 200) {
-      dispatch(authenticate())
-      Router.push('/');
-    } else {
-      Router.push('/ap/login')
-    }
-  };
-
-  return (
-      <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="username"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
+const register = () => {
+    return (
+        <div className={styles.container}>
+            <nav className={styles.nav}>
+                <Link href="/" passHref>
+                    <h1>kuzaHub</h1>
                 </Link>
-              </Grid>
-              <Grid item>
-                <Link href="../nu/register" variant="body2">
-                  {"Don't have an account? Sign Up"}
+                
+            </nav>
+
+            <Divider />
+
+            <main className={styles.main}>
+                <h2>Log In to kuzaHub</h2>
+
+                <Divider />
+
+                <div className={styles.email}>
+                    <EmailIcon />
+                    <input type="email" placeholder="email address" name='username' />
+                </div>
+
+                <a className={styles.submit} type="submit" > Continue with Email </a>
+
+
+                <Divider>or</Divider>
+
+                <Link href="/" passHref>
+                  <div className={styles.google}>
+                      <GoogleIcon />
+                      <a>Continue With Google</a>
+                  </div>
                 </Link>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
-        <Copyright sx={{ mt: 8, mb: 4 }} />
-      </Container>
-    </ThemeProvider>
-  );
+
+                <Divider />
+
+                <p>
+                    Don&#39;t have an Account yet?  
+                    <Link href='../nu/register'>
+                        <a style={{color: 'green'}}> Sign Up </a> 
+                    </Link>
+                </p> 
+                
+            </main>
+
+            <footer className={styles.footer}>
+                <h3>kuzaHub Footer</h3>
+            </footer>
+        </div>
+    )
 }
 
-export default Login;
+export default register
